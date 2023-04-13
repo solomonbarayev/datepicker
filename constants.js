5c2e793702980b174548e7b53b69eeea914351d7
סולומון ברייב <solomonb@ordaprint.local>
2023-04-13 15:58:06 +0300

    push all updates

---
+++ constants.js
@@ -0,0 +1,34 @@
+// add months in hebrew to dropdown menu
+export const months = [
+  "ינואר",
+  "פברואר",
+  "מרץ",
+  "אפריל",
+  "מאי",
+  "יוני",
+  "יולי",
+  "אוגוסט",
+  "ספטמבר",
+  "אוקטובר",
+  "נובמבר",
+  "דצמבר",
+];
+
+//arrary of months in hebrew short names
+export const monthsShort = [
+  "ינו",
+  "פבר",
+  "מרץ",
+  "אפר",
+  "מאי",
+  "יוני",
+  "יולי",
+  "אוג",
+  "ספט",
+  "אוק",
+  "נוב",
+  "דצמ",
+];
+
+//array of days in hebrew short names
+export const daysShort = ["א", "ב", "ג", "ד", "ה", "ו", "ש"];
